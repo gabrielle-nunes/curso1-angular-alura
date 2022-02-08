186b25410915909703fbd90c7ab41492cbe6a005
Gabrielle <gabinunes09@gmail.com>
2022-02-08 14:41:48 -0300

    Primeiro component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NovaTransferecnaiComponent } from './nova-transferencia/nova-transferencia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NovaTransferecnaiComponent
   ],
   imports: [
     BrowserModule
