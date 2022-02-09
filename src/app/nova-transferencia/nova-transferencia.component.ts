fae2ff36072b405190f3c59552f402fd0edc1350
Gabrielle <gabinunes09@gmail.com>
2022-02-09 13:36:31 -0300

    Exportando dados do componente

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
-@Component({
-    selector:'app-nova-transferencia',
-    templateUrl:'./nova-transferencia.component.html',
-    styleUrls:['./nova-transferencia.component.scss']
-})
-export class NovaTransferecnaiComponent{
+import { Component, EventEmitter, Output } from '@angular/core';
 
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
+})
+
+export class NovaTransferecnaiComponent {
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number;
+  destino: number;
+
+  transferir() {
+    console.log('Solicitada nova transferência');
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.aoTransferir.emit(valorEmitir);
+    this.limparCampos();
+  }
+
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
+  }
 }
